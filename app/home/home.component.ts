b519f06a87518e77d5b3d21548457ad99b0e8e7d
zawmyohtut <zaw@intersective.com>
2016-08-30 16:55:44 +1000

    - added home component

---
+++ app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'zaw-home',
+  template: '<h1>This is home screen!</h1>'
+})
+
+export class HomeComponent { 
+   
+}
